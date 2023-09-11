3b5cbf82e2d6db9a2f934d4ca1c54fa3c2bb14c1
PrathameshShingateLy <Prathamesh@ly>
2023-09-12 00:15:00 +0530

    Environment and initial folder structure setup completed

---
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,5 @@
+const DashboardPage = () => {
+  return <h2 className="text-2xl">Dashboard page</h2>;
+};
+
+export default DashboardPage;
---
+++ app/(landing)/page.tsx
@@ -0,0 +1,7 @@
+import { Button } from "@/components/ui/button";
+
+const LandingPage = () => {
+  return <Button variant="default">Click me</Button>;
+};
+
+export default LandingPage;
